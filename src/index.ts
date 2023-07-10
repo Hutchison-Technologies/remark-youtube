55bbc49822d86dbd0b4a816e1ca5776180f44a7d
Jeff Hughes <jeff.hughes@hutchison-t.com>
2023-07-10 11:32:19 +0100

    updated to be node v16 compatible

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import type { Root, Text, Link } from 'mdast';
-import { visit } from 'unist-util-visit';
+import type { Node, Parent } from 'unist';
+import {visit} from 'unist-util-visit';
 
 const DEFAULT_WIDTH = 560;
 const DEFAULT_HEIGHT = 315;
@@ -11,31 +12,33 @@ interface Options {
 }
 
 const remarkYoutubePlugin = (options?: Options) => (tree: Root) => {
-  visit(tree, 'paragraph', (node) => {
+  visit(tree as Parent, 'paragraph', (node: Node & { children?: Node[] }) => {
     let videoId = '';
     let videoUrl = '';
-    for (const child of node.children) {
-      // parse type = 'link' for 'remark-gfm'
-      if (child.type === 'text' || child.type === 'link') {
-        const url = (child as Link)?.url ?? (child as Text)?.value;
-        const match = url.match(URL_PATTERN);
-        if (match && match[1]) {
-          videoId = match[1];
-          videoUrl = url;
-          break;
+    if (Array.isArray(node.children)) {
+      for (const child of node.children) {
+        // parse type = 'link' for 'remark-gfm'
+        if (child.type === 'text' || child.type === 'link') {
+          const url = child.type === 'link' ? (child as any).url : (child as any).value;
+          const match = url.match(URL_PATTERN);
+          if (match && match[1]) {
+            videoId = match[1];
+            videoUrl = url;
+            break;
+          }
         }
       }
     }
 
     if (videoId && videoUrl) {
-      const text: Text = {
+      const text = {
         type: 'text',
         value: videoUrl,
         data: {
           hName: 'iframe',
           hProperties: {
-            width: options?.width ?? DEFAULT_WIDTH,
-            height: options?.height ?? DEFAULT_HEIGHT,
+            width: options?.width || DEFAULT_WIDTH,
+            height: options?.height || DEFAULT_HEIGHT,
             src: `https://www.youtube.com/embed/${videoId}`,
             frameborder: '0',
             allow:
@@ -45,9 +48,11 @@ const remarkYoutubePlugin = (options?: Options) => (tree: Root) => {
           hChildren: [],
         },
       };
-      node.children = [text];
+      if (Array.isArray(node.children)) {
+        node.children = [text];
+      }
     }
-  });
-};
+  })
+}
 
 export default remarkYoutubePlugin;
